971fef9d65c8b69148c5e96625ca7d58e0475aac
Aliaksandr Khudnitski <alekskhudnitskiy@gmail.com>
2020-10-25 17:23:07 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -35,10 +35,51 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '**********': ' '
 };
 
+// function decode(expr) {
+//     let splittedExpr = expr.split('**********');
+//     let result = '';
+
+//     let decodeOneWord = (encodedWord) => {
+//         if (encodedWord.length === 0) {
+//             result += ' ';
+//             return;
+//         } else {
+//             let firstSymbol = encodedWord.substring(0, 10);
+//             encodedWord = encodedWord.substring(10);
+
+//             let morseSymbol = firstSymbol.replace(/00/g, '')
+//                                         .replace(/10/g, '.')
+//                                         .replace(/11/g, '-');
+
+//             result += MORSE_TABLE[morseSymbol];
+
+//             return decodeOneWord(encodedWord);
+//         }
+//     }
+
+//     splittedExpr.forEach(el => decodeOneWord(el));
+
+//     return result.trim();
+// }
+
 function decode(expr) {
-    // write your solution here
+    let encodedSymbolsArr = [];
+
+    for (let i = 0; i < expr.length; i += 10) {
+        encodedSymbolsArr.push(expr.slice(i, i + 10));
+    }
+
+    let morseCharsArr = encodedSymbolsArr.map(el => el.replace(/00/g, '')
+        .replace(/11/g, '-')
+        .replace(/10/g, '.')
+    );
+
+    let result = morseCharsArr.map(el => MORSE_TABLE[el]).join('');
+
+    return result;
 }
 
 module.exports = {
